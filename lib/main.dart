5512f4f4d6903e02ccd33b0693ed46e3508813aa
Bruno Eugênio <brunoxgeniusx@gmail.com>
2020-05-19 23:16:04 -0300

    adição dos campos para entrada de dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -37,7 +38,7 @@ class MyHomePage extends StatelessWidget{
         title: Text('Despesas Pessoais'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -52,18 +53,79 @@ class MyHomePage extends StatelessWidget{
                 child: Row(
                   children: <Widget>[
                     Container(
-                      child: Text(tr.value.toString()),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        'R\$ ${tr.value.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20, 
+                          color: Colors.purple
+                        ),
+                      ),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(tr.title),
-                        Text(tr.date.toString()),
+                        Text(
+                          tr.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        
+                        ),
+                        Text(
+                          DateFormat('d MMMM y').format(tr.date),
+                          style: TextStyle(
+                            color: Colors.grey[850],
+                          ),
+                        ),
                       ],
                     )
                   ],
                 )
               );
             }).toList(),
+          ),
+          Card(
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Título',
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Valor (R\$)'
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      FlatButton(
+                        onPressed: (){}, 
+                        child: Text('Nova Transação'),
+                        textColor: Colors.purple,
+                      )
+                    ],
+                  )
+                ]
+              ),
+            ),
           )
         ],
       ),
