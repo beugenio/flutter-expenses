1d6ed83ce3c5caba648b0391380cbbe6bf1ccf07
Bruno Eugênio <brunoxgeniusx@gmail.com>
2020-05-20 12:27:02 -0300

    refatoração na arvore de componentes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:expenses/components/transaction_user.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -15,21 +14,6 @@ class ExpensesApp extends StatelessWidget{
 }
 
 class MyHomePage extends StatelessWidget{
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Bota creek para o chile',
-      value: 56.00,
-      date: DateTime.now()
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de luz',
-      value: 123.00,
-      date: DateTime.now()
-    )
-  ];
-
 
   @override 
   Widget build (BuildContext context){
@@ -47,86 +31,7 @@ class MyHomePage extends StatelessWidget{
               elevation: 6,
             ),
           ),
-          Column(
-            children: _transactions.map((tr) {
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        ),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        'R\$ ${tr.value.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20, 
-                          color: Colors.purple
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          tr.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        
-                        ),
-                        Text(
-                          DateFormat('d MMMM y').format(tr.date),
-                          style: TextStyle(
-                            color: Colors.grey[850],
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                )
-              );
-            }).toList(),
-          ),
-          Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Título',
-                    ),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Valor (R\$)'
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      FlatButton(
-                        onPressed: (){}, 
-                        child: Text('Nova Transação'),
-                        textColor: Colors.purple,
-                      )
-                    ],
-                  )
-                ]
-              ),
-            ),
-          )
+          TransactionUser(),
         ],
       ),
     );
