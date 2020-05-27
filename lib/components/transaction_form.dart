5a75d5d00e9e4ea56c7729e4661c6de6d19c5960
Bruno Eugênio <brunoxgeniusx@gmail.com>
2020-05-27 11:37:05 -0300

    Ajustes para mensagem modal e cores via tema

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
-class TransactionForm extends StatelessWidget {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-  
+class TransactionForm extends StatefulWidget {
   final void Function(String, double) onSubmit;
 
   TransactionForm(this.onSubmit);
+
+  @override
+  _TransactionFormState createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
 
   _submitForm(){
 
@@ -16,10 +21,10 @@ class TransactionForm extends StatelessWidget {
     if(title.isEmpty || value <= 0.0){
       return;
     }                      
-    onSubmit(title,value);
+    widget.onSubmit(title,value);
 
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Card(
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:expenses/components/transaction_form.dart';
 import 'package:flutter/material.dart';
-import 'package:expenses/components/transaction_user.dart';
+
+import 'dart:math';
+import 'components/transaction_form.dart';
+import 'components/transaction_list.dart';
+import 'models/transaction.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -7,14 +12,63 @@ class ExpensesApp extends StatelessWidget{
   @override 
   Widget build (BuildContext context){
     return MaterialApp(
-      home: MyHomePage()
+      home: MyHomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+      ),
 
     );
   }
 }
 
-class MyHomePage extends StatelessWidget{
+class MyHomePage extends StatefulWidget{
   
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+     final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Bota creek para o chile',
+      value: 56.00,
+      date: DateTime.now()
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de luz 0',
+      value: 123.00,
+      date: DateTime.now()
+    ),
+  ];
+
+   _addTransaction(String title, double value){
+    final newTransaction = Transaction(
+        id: Random().nextDouble().toString(), 
+        title: title, 
+        value: value, 
+        date: DateTime.now()
+      );
+
+      setState(() {
+        _transactions.add(newTransaction);
+      });
+
+      Navigator.of(context).pop(); 
+  }
+
+  _openTransactionFormModal(BuildContext context){
+    showModalBottomSheet(
+      context: context, 
+      builder: (_){
+        return TransactionForm(_addTransaction);
+      }
+    );
+
+  }
 
   @override 
   Widget build (BuildContext context){
@@ -24,7 +78,7 @@ class MyHomePage extends StatelessWidget{
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.account_balance_wallet), 
-            onPressed: (){},
+            onPressed: () => _openTransactionFormModal(context),
           )
         ],
       ),
@@ -39,13 +93,13 @@ class MyHomePage extends StatelessWidget{
                 elevation: 6,
               ),
             ),
-            TransactionUser(),
+            TransactionList(_transactions),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.account_balance_wallet),
-        onPressed: (){},
+        onPressed: () => _openTransactionFormModal(context),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
