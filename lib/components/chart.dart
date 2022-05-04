aa1d0e72c96d7a9b871ed6bb53310edd6cbcdfe4
Bruno Eugenio <beugenio@Mac-mini-de-Bruno.local>
2022-05-04 15:14:33 -0300

    ajustes para funcionar na versão 2.x do Flutter

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -4,42 +4,38 @@ import 'package:intl/intl.dart';
 import 'chart_bar.dart';
 
 class Chart extends StatelessWidget {
-  
   final List<Transaction> recentTransaction;
 
   Chart(this.recentTransaction);
 
-  List<Map<String, Object>> get groupedTransactions{
-    return List.generate(7, (index){
+  List<Map<String, dynamic>> get groupedTransactions {
+    return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(
         Duration(days: index),
       );
 
       double totalSum = 0;
-      for (var i= 0; i < recentTransaction.length; i++){
+      for (var i = 0; i < recentTransaction.length; i++) {
         bool sameDay = recentTransaction[i].date.day == weekDay.day;
         bool sameMonth = recentTransaction[i].date.month == weekDay.month;
         bool sameYear = recentTransaction[i].date.year == weekDay.year;
 
-        if (sameDay && sameMonth && sameYear){
+        if (sameDay && sameMonth && sameYear) {
           totalSum += recentTransaction[i].value;
         }
-
       }
       print(DateFormat.E().format(weekDay)[0]);
       print(totalSum);
 
-      return {'day': DateFormat.E().format(weekDay)[0], 
-              'value': totalSum
-      };
+      return {'day': DateFormat.E().format(weekDay)[0], 'value': totalSum};
     }).reversed.toList();
   }
 
-double get _weekTotalValue{
-  return groupedTransactions.fold(0.0, (sum, tr){
-    return sum + tr['value'];
-  });
-}
+  double get _weekTotalValue {
+    return groupedTransactions.fold(0.0, (sum, tr) {
+      return sum + tr['value'];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,13 +47,15 @@ double get _weekTotalValue{
         padding: const EdgeInsets.all(10),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: groupedTransactions.map((tr){
+          children: groupedTransactions.map((tr) {
             return Flexible(
               fit: FlexFit.tight,
               child: ChartBar(
-                tr['day'], 
-                tr['value'], 
-                _weekTotalValue == 0 ? 0 : (tr['value'] as double) / _weekTotalValue,
+                tr['day'],
+                tr['value'],
+                _weekTotalValue == 0
+                    ? 0
+                    : (tr['value'] as double) / _weekTotalValue,
               ),
             );
           }).toList(),
